700422fc8e711e917598011f345330192efa9455
amogh895 <amoghdixit063@gmail.com>
2026-05-03 02:10:30 +0530

    fixed deployment port

--- backend/app.py
+++ backend/app.py
@@ -147,5 +147,6 @@ def predict():
 # =========================
 # RUN SERVER
 # =========================
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
