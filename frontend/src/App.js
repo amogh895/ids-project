1a65b467382c9e8efd8c7a97f2617db7be87250a
amogh895 <amoghdixit063@gmail.com>
2026-05-27 21:44:45 +0530

    connected backend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -13,7 +13,7 @@ const ActivityIcon = () => <svg xmlns="http://www.w3.org/2000/svg" width="20" he
 const UsersIcon = () => <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><path d="M17 21v-2a4 4 0 0 0-4-4H5a4 4 0 0 0-4 4v2"></path><circle cx="9" cy="7" r="4"></circle><path d="M23 21v-2a4 4 0 0 0-3-3.87"></path><path d="M16 3.13a4 4 0 0 1 0 7.75"></path></svg>;
 const TrashIcon = () => <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><polyline points="3 6 5 6 21 6"></polyline><path d="M19 6v14a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2V6m3 0V4a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2v2"></path></svg>;
 
-const API_BASE = "https://ids-project-idf5.onrender.com";
+const API_BASE = "https://ids-project-ohnh.onrender.com" ;
 
 function App() {
   // LOGIN STATE
